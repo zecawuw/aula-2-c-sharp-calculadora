5de920c35c641909bb4bb1fbf321827b83f50166
zecawuw <88804350+zecawuw@users.noreply.github.com>
2023-03-06 19:59:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace zeca_c__aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, resultado;
+            string operador;
+
+            Console.WriteLine("primeiro valor?");
+            valor1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Operador?");
+            operador = Console.ReadLine();
+
+            Console.WriteLine("segundo valor?");
+            valor2 = Convert.ToInt16(Console.ReadLine());
+
+            if(operador == "+"){
+               resultado = valor1 + valor2;
+                Console.WriteLine(resultado);
+            }else if(operador == "-"){
+                resultado = valor1 - valor2;
+                Console.WriteLine(resultado);
+            }else if(operador == "*"){
+                resultado = valor1 * valor2;
+                Console.WriteLine(resultado);
+            }else if(operador == "/"){
+                resultado = valor1 / valor2;
+                Console.WriteLine(resultado);
+            }else{
+                Console.WriteLine("só é aceito os seguntes operadores: *, -, /, +");
+            }
+
+
+        }
+    }
+}
